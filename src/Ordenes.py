e9d0a0ff91d352d5ca8fd0c4b04ecd7878e1996e
CarolinaCCZ <carolina.ccz@hotmail.com>
2020-12-20 04:48:12 +0100

    Cambiar aspecto Ordenes.py
    Se modifica la forma en la que se visualizan los datos

--- src/Ordenes.py
+++ src/Ordenes.py
@@ -2,89 +2,205 @@
 """
 Created on Sat Dec  5 13:28:13 2020
 
-@author: carol
+@author: Carolina Colina Zamorano
 """
 
-
-import sys
 import sqlite3
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QTableWidget, QTableWidgetItem
-from PyQt5.QtCore import QTimer, QTime, Qt
-from PyQt5 import uic
-import Login as lg
-from Ordenes_ui import *
+import sys
+import math
+import time
+import os
+
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtCore import QTimer, QTime
+from PyQt5.QtWidgets import QTableWidgetItem
+from Ordenes_ui import Ui_MainWindow
+from PyQt5.QtCore import QThread
+from PyQt5.QtGui import *
 
 
 
 class OrdenesWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    def __init__(self, *args, **kwargs):
-        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
         # Método encargado de generar la interfaz
-        #self.setupUi(self)
-        uic.loadUi("Ordenes.ui",self)
+        self.setupUi(self)
+        #uic.loadUi("Ordenes.ui", self)
         self.labelOP = QtWidgets.QLabel(self.frame)
         self.setNombreOperario()
         self.setNombreServicio()
         self.setHora()
-        self.mostarOrdenes()
-        
-    
-    
-    # Función para concetar con la base de datos
-    def conectarBD(self):
-        con = sqlite3.connect('Z:\LucErik.db')
-        cur = con.cursor()
-        return cur, con
-        
+        self.generarOrdenes()
+        self.btn_buscarMaterial.clicked.connect(self.buscar)
+        global maquinas, carros
+
+
+    # Función para conecetar con la base de datos
+    @staticmethod
+    def conectarBD():
+        con = sqlite3.connect('Y:\LucErik.db')
+        cursor = con.cursor()
+        return cursor
+
+    def buscar(self):
+        os.system('python BuscarMaterial.py')
+
 
     # Función para añadir el nombre del Operario
     def setNombreOperario(self):
         # Nombre del Operario
         nombreOP = sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3]
         self.nombreOP.setText(nombreOP)
-       
+
     # Función para añadir el nombre del Servicio
     def setNombreServicio(self):
         # Nombre del servicio
         serv = sys.argv[4]
         self.servicio.setText(serv)
-    
+
     # Función que crea el Timer para la hora
     def setHora(self):
         # Creamos el 'Timer'
         timer = QTimer(self)
         timer.timeout.connect(self.displayTime)
         timer.start(1000)
-        
+
     # Función para mostrar la hora
     def displayTime(self):
         currentTime = QTime.currentTime()
         displayText = currentTime.toString('hh:mm:ss')
         self.hora.setText(displayText)
-        
-    # Función que muestra las órdenes en una tabla en pantalla   
-    def mostarOrdenes(self):
+
+    # Función que muestra las órdenes en una tabla en pantalla
+    def generarOrdenes(self):
         # Conexión con la base de datos
-        cur, con = self.conectarBD()
-        cur.execute('SELECT * FROM ORDENES')
-        # Obtengo el listado de las órdenes
-        listaOrdenes = cur.fetchall()
-        
-        # Una vez guardados los datos cierro la conexión con la base de datos
-        con.close()
-        
-        # Añado las órdenes a la tabla
-        for fila in range(len(listaOrdenes)):
-            for columna in range(7):
-                # Si el valor es un int debo convertirlo a string para que lo inserte en la tabla
-                if (type(listaOrdenes[fila][columna]) == int):
-                    celda = str(listaOrdenes[fila][columna])
-                    self.tableWidget.setItem(fila, columna, QTableWidgetItem(celda))
+        global carros_llevar, carros, ubicacion, cub_disponibles
+        carros = 0
+        cub_disponibles = 0
+
+        cursor = OrdenesWindow.conectarBD()
+
+        listaOrdenes = []
+        print(listaOrdenes)
+
+
+        cursor.execute("SELECT * FROM MAQUINAS")
+        maquinas = cursor.fetchall()
+        print("maquinas: ", maquinas)
+
+        for i in range(len(maquinas)):
+            print("")
+            print("Máquina: ", maquinas[i][0])
+            cub = maquinas[i][4] - maquinas[i][5]
+            print("Cubiertas", cub)
+
+            sql = "SELECT * FROM HUECOS WHERE ID ='" + maquinas[i][0] + "'"
+            cursor.execute(sql)
+            huecos = cursor.fetchall()[0]
+            print("Tabla huecos: ", huecos)
+            # print("huecos[0][1]: ", huecos[0][1])
+
+            maquina = maquinas[i][0]
+            material = maquinas[i][2]
+            pendientes = maquinas[i][4] - maquinas[i][5]
+            prioridad = maquinas[i][7]
+            print("prioridad:", prioridad)
+
+            columna = 1
+            totales = 0
+            huecos_totales = maquinas[i][1]
+            huecos_disponibles = 0
+            print("Huecos que hay en total:", huecos_totales)
+
+            while columna < 11:
+                # Si coincide el material
+                if huecos[columna] == maquinas[i][2]:
+                    totales = totales + huecos[columna + 1]
+                    print("Talones totales en la máquina:", totales)
+                    columna = columna + 2
                 else:
-                    self.tableWidget.setItem(fila, columna, QTableWidgetItem(listaOrdenes[fila][columna]))
-        
- 
-        
+                    columna = columna + 2
+                if huecos[columna] == 'VACIO':
+                    huecos_disponibles = huecos_disponibles + 1
+                    print("Huecos vacíos en máquina:", huecos_disponibles)
+
+            totales = totales / 2
+            print("¿Para cuántas cubiertas tengo material?", totales)
+
+            if totales < cub:
+                carros_totales = math.ceil((maquinas[i][4] - maquinas[i][5]) * 2 / 140)
+                print("Carros totales a llevar:", carros_totales)
+
+                columna = 1
+                #cub_disponibles = 0
+
+                ubicacion = ""
+                while columna < 11:
+                    if huecos[columna] == maquinas[i][2]:
+                        cub_disponibles = cub_disponibles + huecos[columna + 1]
+                        print("cub_disponibles:", cub_disponibles)
+                        carros_disponibles = math.floor(cub_disponibles / 140)
+                        print("Carros que hay en máquina:", carros_disponibles)
+                        carros_llevar = carros_totales - carros_disponibles
+                        print("Carros totales a llevar - carros que hay en máquina: ", carros_llevar)
+                        columna = columna + 2
+                    else:
+                        columna = columna + 2
+
+                print("")
+                print("huecos disponibles: ", huecos_disponibles)
+                print("carros a llevar", carros_llevar)
+                if huecos_disponibles > carros_llevar or huecos_disponibles == carros_llevar:
+                    carros = carros_llevar
+                    print("carros", carros)
+                else:
+                    carros = huecos_disponibles
+                    print("carros", carros)
+
+            # AÑADIR A LA TABLA MATERIALES PARA QUE LOS ENCUENTRE
+
+                sql = "SELECT * FROM MATERIALES WHERE CODIGO='" + maquinas[i][2] + "' "
+                cursor.execute(sql)
+                materiales = cursor.fetchall()
+
+                stock = 0
+                for c in range(len(materiales)):
+                    # Guarda en ubicación donde más disponibles haya
+                    print("materiales[c][3]", materiales[c][3])
+                    if materiales[c][3] > stock:
+                        stock = materiales[c][3]
+                        ubicacion = materiales[c][2]
+                        print("ubicacion", ubicacion)
+                        print("stock", stock)
+
+                listaOrdenes.append((maquina, material, carros, ubicacion, stock, cub_disponibles, pendientes, prioridad))
+
+                print("listaOrdenes", listaOrdenes)
+
+        """ ORDENAR LAS ÓRDENES ANTES DE VISUALIZARLAS """
+        # Coloca en primer lugar las que menos material tienen para hacer cubiertas
+        # Si coincide, pone en primer lugar la que mayor prioridad tiene
+        listaOrdenesOrdenada = sorted(listaOrdenes, key=lambda x: (x[5], -x[7]))
+        print("listaOrdenesOrdenada", listaOrdenesOrdenada)
+
+        # Añado las órdenes a la tabla
+        self.visualizarOrdenes(listaOrdenesOrdenada)
+        # self.visualizarOrdenes(listaOrdenes)
+
+    def visualizarOrdenes(self, listaOrdenesOrdenada):
+        # def visualizarOrdenes(self, listaOrdenes):
+        for fila in range(len(listaOrdenesOrdenada)):
+            for columna in range(8):
+                self.tableWidget.setItem(fila, columna, QtWidgets.QTableWidgetItem(str(listaOrdenesOrdenada[fila][columna])))
+                # Si el número de cubiertas que puede hacer con el material que tiene es inferior a 20 cambio el color a amarillo
+                if listaOrdenesOrdenada[fila][5] < 20 or listaOrdenesOrdenada[fila][5] == 20:
+                    self.tableWidget.item(fila, columna).setBackground(QColor("gold"))
+                # Si no hay material disponible en stock cambio a rojo
+                if listaOrdenesOrdenada[fila][4] == 0:
+                    self.tableWidget.item(fila, columna).setBackground(QColor("red"))
+
+
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
     window = OrdenesWindow()
--- src/Ordenes_ui.py
+++ src/Ordenes_ui.py
@@ -7,102 +7,141 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QFont, QIcon, QColor
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QDialog, QPushButton, QTableWidget,
+                             QTableWidgetItem, QAbstractItemView, QHeaderView, QMenu,
+                             QActionGroup, QAction, QMessageBox)
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(951, 656)
+        MainWindow.resize(1055, 656)
+        MainWindow.setWindowTitle("Ordenes")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.frame = QtWidgets.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(0, 0, 951, 71))
+        self.frame.setGeometry(QtCore.QRect(0, 0, 1055, 71))
         self.frame.setStyleSheet("background-color: rgb(230, 233, 236);")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
+
         self.labelOP = QtWidgets.QLabel(self.frame)
-        self.labelOP.setGeometry(QtCore.QRect(30, 20, 101, 21))
+        self.labelOP.setGeometry(QtCore.QRect(60, 20, 101, 21))
         self.labelOP.setStyleSheet("color: rgb(85, 0, 127);\n"
-"text-decoration: underline;\n"
-"font: 75 12pt \"MS Shell Dlg 2\";")
+                                   "text-decoration: underline;\n"
+                                   "font: 75 12pt \"MS Shell Dlg 2\";")
         self.labelOP.setObjectName("labelOP")
+
         self.labelSRV = QtWidgets.QLabel(self.frame)
-        self.labelSRV.setGeometry(QtCore.QRect(430, 20, 101, 21))
+        self.labelSRV.setGeometry(QtCore.QRect(470, 20, 101, 21))
         self.labelSRV.setStyleSheet("color: rgb(85, 0, 127);\n"
-"text-decoration: underline;\n"
-"font: 75 12pt \"MS Shell Dlg 2\";")
+                                    "text-decoration: underline;\n"
+                                    "font: 75 12pt \"MS Shell Dlg 2\";")
         self.labelSRV.setTextFormat(QtCore.Qt.PlainText)
         self.labelSRV.setObjectName("labelSRV")
+
         self.labelHR = QtWidgets.QLabel(self.frame)
-        self.labelHR.setGeometry(QtCore.QRect(730, 20, 61, 21))
+        self.labelHR.setGeometry(QtCore.QRect(770, 20, 61, 21))
         self.labelHR.setStyleSheet("color: rgb(85, 0, 127);\n"
-"text-decoration: underline;\n"
-"font: 75 12pt \"MS Shell Dlg 2\";")
+                                   "text-decoration: underline;\n"
+                                   "font: 75 12pt \"MS Shell Dlg 2\";")
         self.labelHR.setObjectName("labelHR")
+
         self.nombreOP = QtWidgets.QLabel(self.frame)
-        self.nombreOP.setGeometry(QtCore.QRect(150, 20, 211, 21))
+        self.nombreOP.setGeometry(QtCore.QRect(170, 20, 211, 21))
         self.nombreOP.setStyleSheet("color: rgb(85, 0, 127);\n"
-"font: 11pt \"MS Shell Dlg 2\";")
+                                    "font: 11pt \"MS Shell Dlg 2\";")
         self.nombreOP.setTextFormat(QtCore.Qt.AutoText)
         self.nombreOP.setObjectName("nombreOP")
+
         self.servicio = QtWidgets.QLabel(self.frame)
-        self.servicio.setGeometry(QtCore.QRect(540, 20, 111, 21))
+        self.servicio.setGeometry(QtCore.QRect(570, 20, 111, 21))
         self.servicio.setStyleSheet("color: rgb(85, 0, 127);\n"
-"font: 11pt \"MS Shell Dlg 2\";")
+                                    "font: 11pt \"MS Shell Dlg 2\";")
         self.servicio.setObjectName("servicio")
+
         self.hora = QtWidgets.QLabel(self.frame)
-        self.hora.setGeometry(QtCore.QRect(800, 20, 111, 21))
+        self.hora.setGeometry(QtCore.QRect(840, 20, 111, 21))
         self.hora.setStyleSheet("color: rgb(85, 0, 127);\n"
-"font: 11pt \"MS Shell Dlg 2\";")
+                                "font: 11pt \"MS Shell Dlg 2\";")
         self.hora.setObjectName("hora")
+
         self.frame_2 = QtWidgets.QFrame(self.centralwidget)
-        self.frame_2.setGeometry(QtCore.QRect(0, 70, 961, 561))
+        self.frame_2.setStyleSheet("background-color: rgb(230, 233, 236);")
+        self.frame_2.setGeometry(QtCore.QRect(0, 70, 1055, 561))
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
-        self.tableWidget = QtWidgets.QTableWidget(self.frame_2)
-        self.tableWidget.setGeometry(QtCore.QRect(0, 0, 951, 501))
-        self.tableWidget.setStyleSheet("")
+
+        self.tableWidget = QTableWidget(self.frame_2)
+        self.tableWidget.setGeometry(QtCore.QRect(15, 0, 1023, 501))
+        self.tableWidget.setStyleSheet("font: 10pt \"MS Shell Dlg 2\";")
         self.tableWidget.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
         self.tableWidget.setRowCount(4)
         self.tableWidget.setObjectName("tableWidget")
-        self.tableWidget.setColumnCount(7)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(0, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(1, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(2, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(3, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(4, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(5, item)
-        item = QtWidgets.QTableWidgetItem()
-        self.tableWidget.setHorizontalHeaderItem(6, item)
+        self.tableWidget.setColumnCount(8)
+
         self.frame_3 = QtWidgets.QFrame(self.frame_2)
-        self.frame_3.setGeometry(QtCore.QRect(0, 500, 951, 61))
+        self.frame_3.setGeometry(QtCore.QRect(0, 500, 1051, 61))
         self.frame_3.setStyleSheet("background-color: rgb(230, 233, 236);")
         self.frame_3.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_3.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_3.setObjectName("frame_3")
+
         self.btn_llevarMaterial = QtWidgets.QPushButton(self.frame_3)
-        self.btn_llevarMaterial.setGeometry(QtCore.QRect(630, 10, 131, 41))
-        self.btn_llevarMaterial.setStyleSheet("background-color:rgb(164, 164, 255);\n"
-"color: rgb(0, 0, 0);\n"
-"font: 11pt \"MS Shell Dlg 2\";")
+        self.btn_llevarMaterial.setText("Llevar Material")
+        self.btn_llevarMaterial.setGeometry(QtCore.QRect(780, 10, 131, 41))
+        self.btn_llevarMaterial.setStyleSheet("background-color:\"steelblue\";\n"
+                                              "color: \"white\";\n"
+                                              "font: 11pt \"MS Shell Dlg 2\";")
         self.btn_llevarMaterial.setObjectName("btn_llevarMaterial")
-        self.btn_buscarrMaterial = QtWidgets.QPushButton(self.frame_3)
-        self.btn_buscarrMaterial.setGeometry(QtCore.QRect(140, 10, 141, 41))
-        self.btn_buscarrMaterial.setStyleSheet("background-color:rgb(164, 164, 255);\n"
-"font: 11pt \"MS Shell Dlg 2\";\n"
-"color: rgb(0, 0, 0)")
-        self.btn_buscarrMaterial.setObjectName("btn_buscarrMaterial")
+
+        self.btn_buscarMaterial = QtWidgets.QPushButton(self.frame_3)
+        self.btn_buscarMaterial.setText("Buscar Material")
+        self.btn_buscarMaterial.setGeometry(QtCore.QRect(200, 10, 141, 41))
+        self.btn_buscarMaterial.setStyleSheet("background-color:\"steelblue\";\n"
+                                              "color: \"white\";\n"
+                                              "font: 11pt \"MS Shell Dlg 2\";")
+        self.btn_buscarMaterial.setObjectName("btn_buscarMaterial")
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+
+        # Deshabilitar edición
+        self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
+
+        # Deshabilitar el comportamiento de arrastrar y soltar
+        self.tableWidget.setDragDropOverwriteMode(False)
+
+        # Seleccionar toda la fila
+        self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        # Alineación del texto del encabezado
+        self.tableWidget.horizontalHeader().setDefaultAlignment(Qt.AlignHCenter | Qt.AlignVCenter |
+                                                                Qt.AlignCenter)
+
+        # Deshabilitar resaltado del texto del encabezado al seleccionar una fila
+        self.tableWidget.horizontalHeader().setHighlightSections(False)
+
+        # Ocultar encabezado vertical
+        self.tableWidget.verticalHeader().setVisible(False)
+
+        nombreColumnas = (
+        "MÁQUINA", "MATERIAL", "CARROS", "UBICACIÓN", "DISPONIBLES", "CUBIERTAS", "PENDIENTES", "PRIORIDAD")
+        # Establecer las etiquetas de encabezado horizontal usando etiquetas
+        self.tableWidget.setHorizontalHeaderLabels(nombreColumnas)
+
+        # Color del encabezado
+        style = "::section {""background-color: steelblue; color: white; }"
+        self.tableWidget.horizontalHeader().setStyleSheet(style)
+
+        # Color de fondo de la tabla
+        self.tableWidget.setStyleSheet("background-color: ghostwhite; color: black;")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -113,23 +152,5 @@ class Ui_MainWindow(object):
         self.labelOP.setText(_translate("MainWindow", "OPERARIO:"))
         self.labelSRV.setText(_translate("MainWindow", "SERVICIO:"))
         self.labelHR.setText(_translate("MainWindow", "HORA:"))
-        self.nombreOP.setText(_translate("MainWindow", "Carolina Colina Zamorano"))
-        self.servicio.setText(_translate("MainWindow", "servicio"))
-        self.hora.setText(_translate("MainWindow", "hora"))
-        item = self.tableWidget.horizontalHeaderItem(0)
-        item.setText(_translate("MainWindow", "MÁQUINA"))
-        item = self.tableWidget.horizontalHeaderItem(1)
-        item.setText(_translate("MainWindow", "MATERIAL"))
-        item = self.tableWidget.horizontalHeaderItem(2)
-        item.setText(_translate("MainWindow", "CARROS"))
-        item = self.tableWidget.horizontalHeaderItem(3)
-        item.setText(_translate("MainWindow", "UBICACIÓN"))
-        item = self.tableWidget.horizontalHeaderItem(4)
-        item.setText(_translate("MainWindow", "DISPONIBLES"))
-        item = self.tableWidget.horizontalHeaderItem(5)
-        item.setText(_translate("MainWindow", "CUBIERTAS"))
-        item = self.tableWidget.horizontalHeaderItem(6)
-        item.setText(_translate("MainWindow", "FABRICADAS"))
-        self.btn_llevarMaterial.setText(_translate("MainWindow", "Llevar Material"))
-        self.btn_buscarrMaterial.setText(_translate("MainWindow", "Buscar Material"))
+
 
